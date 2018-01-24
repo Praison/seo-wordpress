86ceaadbecb023876d320fe3dddab8d12a8c0aea
mervinpraison <mervinpraison@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-01-24 16:55:53 +0000

    Merge branch 'master' into svnsync
    git-svn-id: https://plugins.svn.wordpress.org/seo-wordpress/trunk@1808767 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seo-wordpress.php
+++ seo-wordpress.php
@@ -2,17 +2,17 @@
 /**
  * @package Praison SEO
  * @author Mervin Praison
- * @version 4.0.8
+ * @version 4.0.9
  */
 /*
     Plugin Name: Praison SEO
     Plugin URI: http://mer.vin/seo-wordpress/
     Description: SEO Wordpress Plugin by Mervin Praison is a Powerfull Best Optimisation Plugin which has many SEO Features. Google Webmasters and Google Analytics Integration. Very Easy to Setup. Check all benefits here https://mer.vin/seo-wordpress/
     Author: Mervin Praison
-    Version: 4.0.8
+    Version: 4.0.9
     License: GPL
     Author URI: https://seomanageruk.com/
-    Last change: 23.01.2018
+    Last change: 24.01.2018
 */
 
 
